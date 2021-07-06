099f8889cc060663ec695ed951cc0b4d53ca95f7
fauzi0413 <87017004+fauzi0413@users.noreply.github.com>
2021-07-06 19:18:11 +0700

    first project

---
+++ KopiKuy/index.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KopiKuy</title>
+    <link rel="icon" href="./image/logo1.png">
+    <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+<body>
+    <header>
+        <div class="header">
+            <div class="header-left">
+                <h2>KopiKuy</h2>
+            </div>
+
+            <div class="header-right">
+                <ul>
+                    <li><a href="#" class="cool-link">HOME</a></li>
+                    <li><a href="#about" class="cool-link">ABOUT</a></li>
+                    <li><a href="#product" class="cool-link">PRODUCT</a></li>
+                    <li><a href="#contact" class="cool-link">CONTACT</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+    <div class="container">
+        <div class="main">
+            <div class="jumbotron">
+                <div class="quote">
+                    <h1>KopiKuy</h1>
+                    <p>" Kopi modern dengan gaya klasik, Kopi dulu kuyy "</p>
+                </div>
+                <div class="picture">
+                    <img src="./image/logo1.png" alt="Logo">
+                </div>
+            </div >
+            
+            <!-- ABOUT MENU -->
+            <div id="about" class="content">
+                <div class="about">
+                    <div class="about-card">
+                        <div class="about-content">
+                            <h1>ABOUT US</h1>
+                            <div class="content-main">
+                                <div class="content-left">
+                                    <h2>KopiKuy</h2>
+                                    <p>&nbsp;&nbsp;&nbsp;&nbsp;KopiKuy menyajikan beberapa produk kopi pilihan terbaik dari Indonesia. Kenapa hanya kopi dari Indonesia?</p>
+                                    <p>&nbsp;&nbsp;&nbsp;&nbsp;Karena KopiKuy ingin mengenalkan lebih lanjut tentang macam-macam kopi yang ada di Indonesia yang kualitasnya termasuk terbaik di dunia</p>
+                                </div>
+                                <div class="content-right">
+                                    <h2>VISI</h2>
+                                    <p>Menjadikan produk kopi asal Indonesia sebagai specialty coffee terbaik di dunia.</p>
+                                    <h2>MISI</h2>
+                                    <ul>
+                                        <li>Memperkenalkan berbagai jenis specialty coffee Indonesia untuk dikonsumsi oleh masyarakat Indonesia dan dunia.</li>
+                                        <li>Meningkatkan kreativitas untuk menciptakan menu-menu baru yang lebih bervariatif untuk dapat ditawarkan dan dinikmati oleh pelanggan</li>
+                                        <li>Mampu memperkenalkan kopi Indonesia agar dapat bersaing dan dikenal di dunia.</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- AKHIR ABOUT MENU -->
+            
+
+            <!-- PRODUCT MENU -->
+            <div id="product" class="content">
+                <div class="product">
+                    <div class="product-card">
+                        <h1>PRODUCT MENU</h1>
+                        <div class="product-space">
+
+                            <div class="border">
+                                <div class="product-content">
+                                    <div class="image">
+                                        <img src="./image/coffeeProduct.png" alt="Product">
+                                    </div>
+                                    <div class="description">
+                                        <h3>Kopi Arabika Gayo Aceh</h3>
+                                        <p>&nbsp;&nbsp;&nbsp;Kopi gayo merupakan varietas kopi arabika yang menjadi salah satu komoditi unggulan yang berasal dari dataran tinggi Gayo, Aceh Tengah, Indonesia. 
+                                        Kopi gayo juga sudah mendapatkan banyak prestasi yang memantapkan posisi kopi gayo menjadi kopi organik terbaik dunia.</p>
+                                    </div>
+                                </div>
+
+                                <div class="pesan">
+                                    <a href="https://wa.me/6281908279448?text=Hallo, saya ingin memesan Kopi Arabika Gayo Aceh" target="_blank"><span>PESAN</span></a>
+                                </div>
+                            </div>
+                            
+
+                            <div class="border">
+                                <div class="product-content">
+                                    <div class="image">
+                                        <img src="./image/coffeeProduct2.png" alt="Product">
+                                    </div>
+                                    <div class="description">
+                                        <h3>Kopi Arabika Kintamani</h3>
+                                        <p>&nbsp;&nbsp;&nbsp;Kopi Arabika Bali Kintamani memiliki cita rasa khas yang berbeda dibanding kopi jenis lainnya. Terbuat dari biji kopi dengan proses natural, menjadikan rasa khas dan keaslian kopi Bali yang sulit didapat dari kopi lainnya. 
+                                        Oleh karena itu, kopi ini wajib untuk kamu coba</p>
+                                    </div>
+                                </div>
+                                
+                                <div class="pesan">
+                                    <a href="https://wa.me/6281908279448?text=Hallo, saya ingin memesan Kopi Arabika Kintamani" target="_blank"><span>PESAN</span></a>
+                                </div>
+                            </div>
+
+                            <div class="border">
+                                <div class="product-content">
+                                    <div class="image">
+                                        <img src="./image/coffeeProduct3.png" alt="Product">
+                                    </div>
+                                    <div class="description">
+                                        <h3>Kopi Arabika Toraja</h3>
+                                        <p>&nbsp;&nbsp;&nbsp;Kopi Toraja adalah jenis kopi Arabica yang disebut “queen of coffe”. Cirinya adalah kopi ini memiliki aroma khas seperti aroma tanah. Rasa pahitnya menonjol, 
+                                        namun dengan kadar asam yang rendah, tidak bikin mual ketika meminumnya</p>
+                                    </div>
+                                </div>
+                                
+                                <div class="pesan">
+                                    <a href="https://wa.me/6281908279448?text=Hallo, saya ingin memesan Kopi Arabika Toraja" target="_blank"><span>PESAN</span></a>
+                                </div>
+                            </div>
+
+                            <div class="border">
+                                <div class="product-content">
+                                    <div class="image">
+                                        <img src="./image/coffeeProduct4.png" alt="Product">
+                                    </div>
+                                    <div class="description">
+                                        <h3>Kopi Arabika Java Ijen Raung</h3>
+                                        <p>&nbsp;&nbsp;&nbsp;Satu kontainer kopi Java Ijen Raung khas Bondowoso pertama kali diekspor pada 2011, tapi popularitasnya di luar negeri kian meroket. 
+                                        Hingga 2016, Indonesia berhasil mengekspor 43 kontainer atau setara dengan 858,91 ton kopi.</p>
+
+                                        <p>&nbsp;&nbsp;&nbsp;Kopi Java Ijen Raung punya ciri khas unik yakni rasa sedikit pedas dengan aroma bunga hutan. 
+                                        Tingkat asamnya sedang, tapi cenderung ke asam Jawa daripada citrus.</p>
+                                    </div>
+                                </div>
+                                
+                                <div class="pesan">
+                                    <a href="https://wa.me/6281908279448?text=Hallo, saya ingin memesan Kopi Arabika Toraja" target="_blank"><span>PESAN</span></a>
+                                </div>
+                            </div>
+
+                            <div class="border">
+                                <div class="product-content">
+                                    <div class="image">
+                                        <img src="./image/coffeeProduct5.png" alt="Product">
+                                    </div>
+                                    <div class="description">
+                                        <h3>Kopi Liberika Rangsang Meranti</h3>
+                                        <p>&nbsp;&nbsp;&nbsp;Kopi Liberika Meranti memiliki beberapa ciri khas khusus seperti buah kopinya memiliki ukuran yang lebih besar
+                                        dari pada buah kopi arabika maupun robusta. Hal ini karena kulit buah kopinya sangat tebal, sehingga tidak dapat diproses secara manual.
+                                        Itu lah kenapa kopi yang ada di desa Kedaburapat, Pulau Rangsang, Kabupaten Kepulauan Meranti ini disebut unik.</p>
+                                    </div>
+                                </div>
+                                
+                                <div class="pesan">
+                                    <a href="https://wa.me/6281908279448?text=Hallo, saya ingin memesan Kopi Arabika Toraja" target="_blank"><span>PESAN</span></a>
+                                </div>
+                            </div>
+
+                            <div class="border">
+                                <div class="product-content">
+                                    <div class="image">
+                                        <img src="./image/coffeeProduct6.png" alt="Product">
+                                    </div>
+                                    <div class="description">
+                                        <h3>Kopi Arabika Flores Bajawa</h3>
+                                        <p>&nbsp;&nbsp;&nbsp;Kekentalan cukup pekat, tapi tingkat keasamannya rendah. Berbeda dengan kebanyakan kopi di Indonesia, kopi Arabika dari kawasan Bajawa, Flores, Nusa Tenggara Timur, punya rasa dominan cokelat dan vanili dengan after taste yang sedikit nutty. 
+                                        Amerika adalah negara tujuan sebagian besar ekspor kosmoditas kopi Flores Bajawa.</p>
+                                    </div>
+                                </div>
+                                
+                                <div class="pesan">
+                                    <a href="https://wa.me/6281908279448?text=Hallo, saya ingin memesan Kopi Arabika Toraja" target="_blank"><span>PESAN</span></a>
+                                </div>
+                            </div>
+
+                            <div class="">
+                                <div class="product-content">
+                                    <div class="image">
+                                        <img src="./image/coffeeProduct7.png" alt="Product">
+                                    </div>
+                                    <div class="description">
+                                        <h3>Kopi Robusta Temanggung</h3>
+                                        <p>&nbsp;&nbsp;&nbsp;Aroma yang cukup unik ini, yakni aroma tembakau dengan rasa pahit pekat dan mendominasi. Namun itulah yang membuat kopi khas Temanggung, Jawa Tengah, 
+                                        ini sangat autentik dan digemari. Bahkan pasar ekspornya sudah mencapai negara-negara Eropa, Timur Tengah, hingga Amerika Latin.</p>
+                                    </div>
+                                </div>
+                                
+                                <div class="pesan">
+                                    <a href="https://wa.me/6281908279448?text=Hallo, saya ingin memesan Kopi Arabika Toraja" target="_blank"><span>PESAN</span></a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- AKHIR PRODUCT MENU -->
+            
+
+            <!-- CONTACT MENU -->
+            <div id="contact" class="content">
+                <div class="contact">
+                    <div class="contact-card">
+                        <h1>CONTACT US</h1>
+                        <div class="contact-content">
+                            <div class="contact-left">
+                                <h3>&nbsp;&nbsp;&nbsp;Apabila anda mempunyai Kritik atau Saran bisa menghubungi kami melalui Sosial Media kami ataupun bisa langsung menuliskannya di form.</h3>
+                                <div class="sosmed">
+                                    <div class="logo">
+                                        <div class="card-posisi">
+                                            <span class="whatsapp"><i class="fab fa-whatsapp"></i></span>
+                                            <a href="https://wa.me/6281908279448?text=Hallo, saya ingin memberikan kritik dan saran saya untuk Website KopiKuy" target="_blank"><span class="text-whatsapp"></span></a>
+                                        </div>
+                                    </div>
+                        
+                                    <div class="logo">
+                                        <div class="card-posisi">
+                                            <span><i class="fab fa-instagram"></i></span>
+                                            <a href="https://www.instagram.com/kopikuy20/?hl=id" target="_blank"><span class="text-instagram"></span></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="contact-right">
+                                <form class="form" method="POST" action="">
+                                    <label>Your Name</label>
+                                    <br>
+                                    <input type="text" name="name" class="form-control" maxlength="50" required>
+                                    <br>
+                                    <label>Your Email</label>
+                                    <br>
+                                    <input type="email" name="email" class="form-control" maxlength="200" required>
+                                    <br>
+                                    <label>Your Message</label>
+                                    <br>
+                                    <textarea name="message" rows="5" class="form-control" required></textarea>
+                                    <br>
+                                    <input id="submit" type="submit" name="submit" class="form-control button" value="SEND">
+                                </form>
+
+                                <?php
+                                    if (isset($_POST['submit'])){
+                                        include_once 'function.php';
+                                        $obj = new form();
+                                        $res = $obj->contact($_POST);
+                                        if ($res==true){
+                                            echo 
+                                            "<script>alert('Success'<br>'Terima Kasih Atas Saran dan Kritik Yang Anda Berikan')</script>";
+                                        }
+                                        else{
+                                            echo 
+                                            "<script>alert('Periksa Kembali')</script>";
+                                        }
+                                    }
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- AKHIR CONTACT MENU -->
+
+        </div>
+    </div>
+
+    <footer>
+        <p>&copy; Copyright KopiKuy 2021</p>
+    </footer>
+    <script src="index.js"></script>
+</body>
+</html>
---
+++ KopiKuy/kopikuy.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 01, 2021 at 03:40 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kopikuy`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `message` text NOT NULL,
+  `datetime` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`id`, `name`, `email`, `message`, `datetime`) VALUES
+(1, 'fauzi', '123@gmail.com', 'hello', '2020-12-17 11:11:48'),
+(2, 'fauzi', '123@gmail.com', 'hello', '2020-12-17 11:12:57');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
